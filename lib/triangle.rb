c7179576386d0982c72b38e96774a2e5bf2dcebd
MarlonMo37 <marlonmoreno894@gmail.com>
2020-11-20 19:22:39 -0600

    commit tree 56dd41ec113a6ce2146e47227ff8cfddf5892379

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
-end
+
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if s1 <= 0 || s2 <= 0 || s3 <= 0
+      begin
+        raise TriangleError
+      end
+    elsif s1 + s2 <= s3 || s1 + s3 <= s2 || s2 + s3 <= s1
+      begin 
+        raise TriangleError
+      end
+    elsif s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 == s2 || s2 == s3 || s3 == s1
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    false
+  end
+
+  end
